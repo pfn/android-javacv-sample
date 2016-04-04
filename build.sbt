20a4e9de729b577b68cd3d3911e1e00f315d55a7
Perry Nguyen <pfnguyen@hanhuy.com>
2016-04-04 10:04:46 -0700

    snapshot

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
+libraryDependencies += "com.google.guava" % "guava" % "19.0"
+
 javacOptions in Compile ++= "-source" :: "1.7" :: "-target" :: "1.7" :: Nil
 
 javaCppPresetLibs ++= Seq("opencv" -> "3.0.0", "ffmpeg" -> "2.8.1")
-javaCppPlatform := "android-arm"
+javaCppPlatform := Seq("android-arm")
 
 minSdkVersion := "21"
 targetSdkVersion := "21"
@@ -15,5 +17,5 @@ android.dsl.apkExclude(
   "META-INF/maven/org.bytedeco.javacpp-presets/ffmpeg/pom.xml"
 )
 
-//useProguardInDebug := false
-//protifySettings
+useProguardInDebug := false
+protifySettings
--- src/main/scala/com/hanhuy/android/vision/FacePreview.java
+++ src/main/scala/com/hanhuy/android/vision/FacePreview.java
@@ -1,8 +1,5 @@
 package com.hanhuy.android.vision;
 
-/**
- * @author pfnguyen
- */
 /*
  * Copyright (C) 2010,2011,2012 Samuel Audet
  *
@@ -55,10 +52,7 @@ package com.hanhuy.android.vision;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ImageFormat;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.os.Bundle;
@@ -68,17 +62,24 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
-import java.io.File;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 import java.util.List;
-import org.bytedeco.javacpp.Loader;
-import org.bytedeco.javacpp.opencv_objdetect;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
+import com.google.common.collect.Iterables;
+import org.bytedeco.javacv.AndroidFrameConverter;
+import org.bytedeco.javacv.OpenCVFrameConverter.ToIplImage;
+
+import static org.bytedeco.javacpp.Loader.sizeof;
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_imgproc.*;
-import static org.bytedeco.javacpp.opencv_objdetect.*;
-import static org.bytedeco.javacpp.opencv_imgcodecs.*;
 
 // ----------------------------------------------------------------------
 
@@ -95,10 +96,15 @@ public class FacePreview extends Activity {
         super.onCreate(savedInstanceState);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         // Create our Preview view and set it as the content of our activity.
         try {
             layout = new FrameLayout(this);
+            layout.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN |
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
             faceView = new FaceView(this);
             mPreview = new Preview(this, faceView);
             layout.addView(mPreview);
@@ -113,32 +119,32 @@ public class FacePreview extends Activity {
 
 // ----------------------------------------------------------------------
 
+class FrameConverter {
+    private final AndroidFrameConverter androidConverter = new AndroidFrameConverter();
+    private final ToIplImage iplConverter = new ToIplImage();
+    public IplImage convert(byte[] data, int width, int height) {
+        return iplConverter.convert(androidConverter.convert(data, width, height));
+    }
+    public Bitmap convert(IplImage ipl) {
+        return androidConverter.convert(iplConverter.convert(ipl));
+    }
+}
 class FaceView extends View implements Camera.PreviewCallback {
     public static final int SUBSAMPLING_FACTOR = 4;
 
-    private IplImage grayImage;
-    private CvHaarClassifierCascade classifier;
+    private IplImage mask;
+    private IplImage frame;
+    private IplImage frame2;
     private CvMemStorage storage;
-    private CvSeq faces;
+    private final FrameConverter previewConverter = new FrameConverter();
+    private final FrameConverter hsvConverter = new FrameConverter();
+    private final FrameConverter maskConverter = new FrameConverter();
 
     public FaceView(FacePreview context) throws IOException {
         super(context);
 
-        // Load the classifier file from Java resources.
-        File classifierFile = Loader.extractResource(getClass(),
-                "/haarcascade_frontalface_alt.xml",
-                context.getCacheDir(), "classifier", ".xml");
-        if (classifierFile == null || classifierFile.length() <= 0) {
-            throw new IOException("Could not extract the classifier file from Java resource.");
-        }
-
         // Preload the opencv_objdetect module to work around a known bug.
-        Loader.load(opencv_objdetect.class);
-        classifier = new CvHaarClassifierCascade(cvLoad(classifierFile.getAbsolutePath()));
-        classifierFile.delete();
-        if (classifier.isNull()) {
-            throw new IOException("Could not load the classifier file.");
-        }
+//        Loader.load(opencv_objdetect.class);
         storage = CvMemStorage.create();
     }
 
@@ -155,51 +161,135 @@ class FaceView extends View implements Camera.PreviewCallback {
     protected void processImage(byte[] data, int width, int height) {
         // First, downsample our image and convert it into a grayscale IplImage
         int f = SUBSAMPLING_FACTOR;
-        if (grayImage == null || grayImage.width() != width/f || grayImage.height() != height/f) {
-            grayImage = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 1);
+        if (mask == null || mask.width() != width/f || mask.height() != height/f) {
+            mask = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 1);
         }
-        int imageWidth  = grayImage.width();
-        int imageHeight = grayImage.height();
+        if (frame == null || frame.width() != width/f || frame.height() != height/f) {
+            frame = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 3);
+            frame2 = IplImage.create(width/f, height/f, IPL_DEPTH_8U, 3);
+        }
+        int imageWidth  = frame.width();
+        int imageHeight = frame.height();
         int dataStride = f*width;
-        int imageStride = grayImage.widthStep();
-        ByteBuffer imageBuffer = grayImage.getByteBuffer();
+        int imageStride = frame.widthStep();
+
+        int[] ints = new int[width * height];
+        decodeYUV420SP(ints, data, width, height);
+        ByteBuffer maskBuffer = mask.createBuffer();
+        ByteBuffer frameBuffer = frame.createBuffer();
+        int rgba;
         for (int y = 0; y < imageHeight; y++) {
-            int dataLine = y*dataStride;
-            int imageLine = y*imageStride;
             for (int x = 0; x < imageWidth; x++) {
-                imageBuffer.put(imageLine + x, data[dataLine + f*x]);
+                rgba = ints[y * dataStride + x * f];
+                frameBuffer.put(y * imageStride + x * 3 + 2, (byte)( rgba  >> 24));
+                frameBuffer.put(y * imageStride + x * 3 + 1, (byte)((rgba >> 16) & 0xff));
+                frameBuffer.put(y * imageStride + x * 3,     (byte)((rgba >> 8)  & 0xff));
             }
         }
+        cvCvtColor(frame, frame, CV_BGR2HSV);
+        ByteBuffer hsvBuffer = frame.createBuffer();
+        int hsvStep = frame.widthStep();
+        boolean hsv1, hsv2;
+        for (int y = 0; y < imageHeight; y++) {
+            for (int x = 0; x < imageWidth; x++) {
+                hsv1 = (hsvBuffer.get(y*hsvStep+x*3) & 0xff) > 170;
+                hsv2 = (hsvBuffer.get(y*hsvStep+x*3+2) & 0xff) < 90;
+                maskBuffer.put(y*imageWidth + x, (byte)(hsv1 || hsv2 ? 0xff : 0));
+            }
+        }
+        cvCvtColor(frame, frame, CV_HSV2BGR);
+        cvErode(mask, mask, null, 1);
+        cvDilate(mask, mask, null, 1);
+//        CvSeq contours = new CvSeq();
+//        cvFindContours(mask, storage, contours, sizeof(CvContour.class), CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, cvPoint(0,0));
+//        while (contours != null && contours.total() <= 650) {
+//            contours = contours.h_next();
+//        }
+//        if (contours != null && contours.total() > 650)
+//            cvDrawContours(frame, contours, CV_RGB(100,100,100), CV_RGB(0,255,0), 1, 2, CV_AA, cvPoint(0,0));
 
         cvClearMemStorage(storage);
-        faces = cvHaarDetectObjects(grayImage, classifier, storage, 1.1, 3,
-                CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH);
         postInvalidate();
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
+    private final Paint paint;
+    private final String s = "FacePreview - This side up.";
+    private final float textWidth;
+    {
+        paint = new Paint();
         paint.setColor(Color.RED);
         paint.setTextSize(20);
+        textWidth = paint.measureText(s);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
 
-        String s = "FacePreview - This side up.";
-        float textWidth = paint.measureText(s);
         canvas.drawText(s, (getWidth()-textWidth)/2, 20, paint);
+        Bitmap b = previewConverter.convert(frame);
 
-        if (faces != null) {
-            paint.setStrokeWidth(2);
-            paint.setStyle(Paint.Style.STROKE);
-            float scaleX = (float)getWidth()/grayImage.width();
-            float scaleY = (float)getHeight()/grayImage.height();
-            int total = faces.total();
-            for (int i = 0; i < total; i++) {
-                CvRect r = new CvRect(cvGetSeqElem(faces, i));
-                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
-                canvas.drawRect(x*scaleX, y*scaleY, (x+w)*scaleX, (y+h)*scaleY, paint);
+//        if (faces != null) {
+//            paint.setStrokeWidth(2);
+//            paint.setStyle(Paint.Style.STROKE);
+//            float scaleX = (float)getWidth()/grayImage.width();
+//            float scaleY = (float)getHeight()/grayImage.height();
+//            int total = faces.total();
+//            for (int i = 0; i < total; i++) {
+//                CvRect r = new CvRect(cvGetSeqElem(faces, i));
+//                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
+//                canvas.drawRect(x*scaleX, y*scaleY, (x+w)*scaleX, (y+h)*scaleY, paint);
+//            }
+//        }
+        canvas.save();
+        canvas.scale(-1, 1);
+        if (b != null) {
+            canvas.drawBitmap(b, -canvas.getWidth() + 100, 100, null);
+        }
+        b = maskConverter.convert(mask);
+        if (b != null) {
+            canvas.drawBitmap(b, -canvas.getWidth() + 100, 500, null);
+        }
+        canvas.restore();
+    }
+    static void decodeYUV420SP(int[] rgba, byte[] yuv420sp, int width,
+                                      int height) {
+
+
+        final int frameSize = width * height;
+// define variables before loops (+ 20-30% faster algorithm o0`)
+        int r, g, b, y1192, y, i, uvp, u, v;
+        for (int j = 0, yp = 0; j < height; j++) {
+            uvp = frameSize + (j >> 1) * width;
+            u = 0;
+            v = 0;
+            for (i = 0; i < width; i++, yp++) {
+                y = (0xff & ((int) yuv420sp[yp])) - 16;
+                if (y < 0)
+                    y = 0;
+                if ((i & 1) == 0) {
+                    v = (0xff & yuv420sp[uvp++]) - 128;
+                    u = (0xff & yuv420sp[uvp++]) - 128;
+                }
+
+                y1192 = 1192 * y;
+                r = (y1192 + 1634 * v);
+                g = (y1192 - 833 * v - 400 * u);
+                b = (y1192 + 2066 * u);
+
+// Java's functions are faster then 'IFs'
+                r = Math.max(0, Math.min(r, 262143));
+                g = Math.max(0, Math.min(g, 262143));
+                b = Math.max(0, Math.min(b, 262143));
+
+                // rgb[yp] =  ((r << 6) & 0xff0000) | ((g >> 2) &
+                // 0xff00) | ((b >> 10) & 0xff);
+                // rgba, divide 2^10 ( >> 10)
+                rgba[yp] = ((r << 14) & 0xff000000) | ((g << 6) & 0xff0000)
+                        | ((b >> 2) & 0xff00) | 0xff;
+//                rgba[yp] = 0xff000000 | ((b << 16) & 0xff0000) | ((g << 8) & 0xff00) | (r & 0xff);
             }
         }
     }
+
 }
 
 // ----------------------------------------------------------------------
@@ -223,7 +313,19 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
     public void surfaceCreated(SurfaceHolder holder) {
         // The Surface has been created, acquire the camera and tell it where
         // to draw.
-        mCamera = Camera.open();
+        int count = Camera.getNumberOfCameras();
+        final Camera.CameraInfo info = new Camera.CameraInfo();
+        Optional<Integer> found = Iterables.tryFind(ContiguousSet.create(com.google.common.collect.Range.closedOpen(0, count), DiscreteDomain.integers()),
+                new Predicate<Integer>() {
+                    @Override
+                    public boolean apply(Integer input) {
+                        Camera.getCameraInfo(input, info);
+                        return info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT;
+                    }
+                }
+        );
+        mCamera = found.isPresent() ? Camera.open(found.get()) : Camera.open();
+        mCamera.setDisplayOrientation((info.orientation + 90) % 360);
         try {
             mCamera.setPreviewDisplay(holder);
         } catch (IOException exception) {
@@ -276,7 +378,10 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         return optimalSize;
     }
 
+    boolean parametersSet = false;
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        if (parametersSet) return;
+        parametersSet = true;
         // Now that the size is known, set up the camera parameters and begin
         // the preview.
         Camera.Parameters parameters = mCamera.getParameters();
@@ -284,6 +389,7 @@ class Preview extends SurfaceView implements SurfaceHolder.Callback {
         List<Size> sizes = parameters.getSupportedPreviewSizes();
         Size optimalSize = getOptimalPreviewSize(sizes, w, h);
         parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+//        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
 
         mCamera.setParameters(parameters);
         if (previewCallback != null) {
